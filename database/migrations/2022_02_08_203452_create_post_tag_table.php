65c7253b947240c118171059cbd1b01a12d7fe01
andreafontaneto <andrea.fontaneto@gmail.com>
2022-02-08 21:46:54 +0100

    make post_tag table w/ post_id fk - tag_id fk + migrate

---
+++ database/migrations/2022_02_08_203452_create_post_tag_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /*** questa è la TABELLA PONTE tra POSTS e TAGS ***/
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->id();
+            
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts')
+                ->onDelete('cascade');
+                // all'eliminazione di un post o di un tag viene eliminato in cascata (cascade) il record
+                // "cascade" serve per eliminare tutta la relazione tra i due id
+                // non ha senso tenerne uno solo come con setNull
+
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('tag_id')
+                ->references('id')
+                ->on('tags')
+                ->onDelete('cascade');
+                
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}
