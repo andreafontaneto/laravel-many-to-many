f2809c8a5787ae6b7568a2acc7936f9cd75a859a
andreafontaneto <andrea.fontaneto@gmail.com>
2022-02-08 21:33:15 +0100

    make TagsTableSeeder

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Tag;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['Front End', 'Back End', 'Design', 'UX', 'Laravel', 'VueJs'];
+
+        foreach($tags as $tag){
+
+            $new_tag = new Tag();
+            $new_tag->name = $tag;
+            $new_tag->slug = Str::slug($new_tag->name, '-');
+            $new_tag->save();
+
+        }
+
+    }
+}
